7b746076822a650a5230ed6de0a6f518a51104bc
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-06-20 10:37:32 +0200

    added global variables and modified code accordingly

--- render.js
+++ render.js
@@ -2,7 +2,7 @@
 
 // Draw individual sprite function (0 blue, 1 red, 2 green, 3 yellow)
 function drawSprite(x, y, number, color) {
-  context.drawImage(spriteSheet, number * 10, color * 10 + 10, 10, 10, x, y, 100, 100);
+  context.drawImage(spriteSheet, number * SPRITEPIXELWIDTH, color * SPRITEPIXELHEIGHT + SPRITEPIXELHEIGHT, SPRITEPIXELWIDTH, SPRITEPIXELHEIGHT, x, y, SPRITEWIDTH, SPRITEHEIGHT);
 }
 
 // -----------------------------------------------------------------------
@@ -21,14 +21,14 @@ function renderBoard() {
 function renderHand() {
 
   context.fillStyle = "grey";
-  context.fillRect(0, 700, 2000, 100);
+  context.fillRect(0, CANVASHEIGHT-SPRITEHEIGHT, CANVASWIDTH, SPRITEHEIGHT);
   for (var i = 0; i < game.playerCards[game.currentPlayer].length; i++) {
     var card = game.playerCards[game.currentPlayer][i];
     if(card.dragging) {
       drawSprite(card.posX, card.posY, card.value, card.color);
     }
     else {
-      drawSprite(i*100, 700, card.value, card.color);
+      drawSprite(i*SPRITEWIDTH, CANVASHEIGHT-SPRITEHEIGHT, card.value, card.color);
     }
   }
 }
