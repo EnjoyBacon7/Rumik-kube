0a93c0b0b25de606b470dd95dfc6d7593ebe08d0
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-06-20 11:17:03 +0200

    Added possibility to drag cards on board as well

--- game.js
+++ game.js
@@ -5,7 +5,16 @@
 gameCanvas.onmousedown = function(e) {
   for (var i = 0; i < game.playerCards[game.currentPlayer].length; i++) {
     var card = game.playerCards[game.currentPlayer][i];
+    // For cards in hand
     if (e.offsetX > i*SPRITEWIDTH && e.offsetX < i*SPRITEWIDTH + SPRITEWIDTH && e.offsetY > CANVASHEIGHT-SPRITEHEIGHT && e.offsetY < CANVASHEIGHT) {
+      card.dragging = true;
+      console.log("dragging");
+    }
+  }
+  for (var i = 0; i < game.boardCards.length; i++) {
+    var card = game.boardCards[i][0];
+    // For cards on board
+    if (e.offsetX > card.posX && e.offsetX < card.posX + SPRITEWIDTH && e.offsetY > card.posY && e.offsetY < card.posY + SPRITEHEIGHT) {
       card.dragging = true;
       console.log("dragging");
     }
@@ -16,7 +25,17 @@ gameCanvas.onmousemove = function(e) {
   context.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
 
   for (var i = 0; i < game.playerCards[game.currentPlayer].length; i++) {
+    // For cards in hand
     var card = game.playerCards[game.currentPlayer][i];
+    if (card.dragging) {
+      card.posX = e.offsetX - SPRITEWIDTH/2;
+      card.posY = e.offsetY - SPRITEHEIGHT/2;
+    }
+  }
+
+  for (var i = 0; i < game.boardCards.length; i++) {
+    // For cards on board
+    var card = game.boardCards[i][0];
     if (card.dragging) {
       card.posX = e.offsetX - SPRITEWIDTH/2;
       card.posY = e.offsetY - SPRITEHEIGHT/2;
@@ -42,7 +61,6 @@ gameCanvas.onmouseup = function(e) {
         game.boardCards[game.boardCards.length-1][0].posY = e.offsetY - SPRITEHEIGHT/2;
         
         console.log(game.boardCards[game.boardCards.length-1][0].posX = e.offsetX - SPRITEWIDTH/2, game.boardCards[game.boardCards.length-1][0].posY = e.offsetY - SPRITEHEIGHT/2);
-
         updateDebug();
       }
 
