d4aa23044e4a683cb2f6375411fa858d3f7a2559
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-06-21 12:08:11 +0200

    refactored code using new game variables
    heldCard now holds the index of a card in the drawnCard list instead of a whole copy of the card. Cards are now split in "drawnCards" and "undrawnCards" lists instead of the complicated "playerCards" "boardCards" and "undrawnCards"

--- game.js
+++ game.js
@@ -9,30 +9,16 @@ var mouseIsDown = false;
 gameCanvas.onmousedown = function(e) {
   mouseIsDown = true;
   var draggedCardFound = false;
-  for (var i = 0; i < game.playerCards[game.currentPlayer].length; i++) {
+
+  for (var i = 0; i < game.drawnCards.length; i++) {
     if(draggedCardFound) { break; }
-    var card = game.playerCards[game.currentPlayer][i];
-    // Splice the card from the player's hand
-    game.playerCards[game.currentPlayer].splice(i, 1);
-    // For cards in hand
-    if (e.offsetX > i*SPRITEWIDTH && e.offsetX < i*SPRITEWIDTH + SPRITEWIDTH && e.offsetY > CANVASHEIGHT-SPRITEHEIGHT && e.offsetY < CANVASHEIGHT) {
-      // If the card that was clicked on was found
-      console.log("dragging");
-      draggedCardFound = true;
-      // Set the card to the game's heldCard
-      game.heldCard = card;
-    }
-  }
-  for (var i = 0; i < game.boardCards.length; i++) {
-    if(draggedCardFound) { break; }
-    var card = game.boardCards[i][0];
-    // For cards on board
+    var card = game.drawnCards[i];
     if (e.offsetX > card.posX && e.offsetX < card.posX + SPRITEWIDTH && e.offsetY > card.posY && e.offsetY < card.posY + SPRITEHEIGHT) {
       // If the card that was clicked on was found
       console.log("mouse down");
       draggedCardFound = true;
       // Set the card to the game's heldCard
-      game.heldCard = card;
+      game.heldCard = i;
     }
   }
 
@@ -42,8 +28,8 @@ gameCanvas.onmousedown = function(e) {
 gameCanvas.onmousemove = function(e) {
   if (mouseIsDown){
 
-    game.heldCard.posX = e.offsetX - SPRITEWIDTH/2;
-    game.heldCard.posY = e.offsetY - SPRITEHEIGHT/2;
+    game.drawnCards[game.heldCard].posX = e.offsetX - SPRITEWIDTH/2;
+    game.drawnCards[game.heldCard].posY = e.offsetY - SPRITEHEIGHT/2;
     console.log("moving");
     renderGame();
   }
@@ -53,8 +39,11 @@ gameCanvas.onmouseup = function(e) {
   mouseIsDown = false;
 
   //check whether the card was on the board or not before being dragged
-  if(game.heldCard.onBoard) {
-    //if it was on the board, do nothing
+  if(game.drawnCards[game.heldCard].onBoard) {
+    //if it was on the board, make sure it is not dropped in the player's hand
+    if(game.drawnCards[game.heldCard].posY >= CANVASHEIGHT - SPRITEHEIGHT*2) {
+      game.drawnCards[game.heldCard].posY = CANVASHEIGHT - SPRITEHEIGHT*2;
+    }
   }
   else {
     // Check if card is dropped on board
