d484b05d80d72b68f0da857e31e732d77b7ba5b7
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-06-22 11:24:01 +0200

    implemented changeplayer into debug menu

--- debug.js
+++ debug.js
@@ -5,50 +5,46 @@ function debug() {
   var debugCard = document.createElement('div');
   debugCard.classList.add("container");
   debugCard.innerHTML = `
-    <div id="debugCard" class="card">
-      <div class="card-header">
-        <h3>Debug Menu</h3>
-      </div>
-      <table class="table card-body border-0 p-0">
-        <thead>
-          <tr class="text-center">
-            <th scope="col" class="border-0 h3 fw-bold">Player</th>
-            <th scope="col" colspan="14" class="border-0 h3 fw-bold">Cards</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr id="player0" class="text-center">
-            <th scope="row" class="border-0">Player 0</th>
+<div id="debugCard" class="card">
+  <div class="card-header">
+    <h3>Debug Menu</h3>
+  </div>
+  <table class="table card-body border-0 p-0">
+    <thead>
+      <tr class="text-center">
+        <th scope="col" class="border-0 h3 fw-bold">Player</th>
+        <th scope="col" colspan="14" class="border-0 h3 fw-bold">Cards</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr id="player0" class="text-center">
 
-          </tr>
-          <tr id="player1" class="text-center">
-            <th scope="row" class="border-0">Player 1</th>
+      </tr>
+      <tr id="player1" class="text-center">
 
-          </tr>
-          <tr id="player2" class="text-center">
-            <th scope="row" class="border-0">Player 2</th>
+      </tr>
+      <tr id="player2" class="text-center">
 
-          </tr>
-          <tr id="player3" class="text-center">
-            <th scope="row" class="border-0">Player 3</th>
+      </tr>
+      <tr id="player3" class="text-center">
 
-          </tr>
-          <tr id="bugged" class="text-center">
-            <th scope="row" class="border-0">Bugged</th>
+      </tr>
+      <tr id="bugged" class="text-center">
+        <th scope="row" class="border-0">Bugged</th>
 
-          </tr>
-        </tbody>
-      </table>
-      <table class="table card-body border-0 p-0">
-        <thead>
-          <tr class="text-center">
-            <th scope="col" colspan="15" class="border-0 h3 fw-bold">Undrawn Cards</th>
-          </tr>
-        </thead>
-        <tbody id="undrawn">
-        </tbody>
-      </table>
-    </div>
+      </tr>
+    </tbody>
+  </table>
+  <table class="table card-body border-0 p-0">
+    <thead>
+      <tr class="text-center">
+        <th scope="col" colspan="15" class="border-0 h3 fw-bold">Undrawn Cards</th>
+      </tr>
+    </thead>
+    <tbody id="undrawn">
+    </tbody>
+  </table>
+</div>
   `;
   document.getElementById("debugMenu").appendChild(debugCard);
 }
@@ -58,10 +54,10 @@ function debugTable(game) {
   var colors = ["aqua", "indianred", "lightgreen", "yellow"];
 
   // Clear the table
-  document.getElementById("player0").innerHTML = `<th scope="row" class="border-0">Player 0</th>`;
-  document.getElementById("player1").innerHTML = `<th scope="row" class="border-0">Player 1</th>`;
-  document.getElementById("player2").innerHTML = `<th scope="row" class="border-0">Player 2</th>`;
-  document.getElementById("player3").innerHTML = `<th scope="row" class="border-0">Player 3</th>`;
+  document.getElementById("player0").innerHTML = `<th scope="row" class="border-0"><button onclick="changePlayer(0)" class="btn btn-secondary">Player 0</button></th>`;
+  document.getElementById("player1").innerHTML = `<th scope="row" class="border-0"><button onclick="changePlayer(1)" class="btn btn-secondary">Player 1</button></th>`;
+  document.getElementById("player2").innerHTML = `<th scope="row" class="border-0"><button onclick="changePlayer(2)" class="btn btn-secondary">Player 2</button></th>`;
+  document.getElementById("player3").innerHTML = `<th scope="row" class="border-0"><button onclick="changePlayer(3)" class="btn btn-secondary">Player 3</button></th>`;
   document.getElementById("bugged").innerHTML = `<th scope="row" class="border-0">Bugged</th>`;
   document.getElementById("undrawn").innerHTML = ``;
 
